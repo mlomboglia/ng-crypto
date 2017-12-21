a71f748a4a3e094aa51167397ee78a3dbe7c28b6
Marcos Lomboglia <marcos.lomboglia@projective.biz>
2017-12-21 13:26:15 +0000

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,22 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+    production: false,
+
+    region: 'eu-west-2',
+
+    identityPoolId: 'eu-west-2:1b62fb94-56d8-4332-99e5-26956ebbce48',
+    userPoolId: 'eu-west-2_R2yYbkkro',
+    clientId: '120p3mu5pi01638ha560aklbmf',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'eu-west-2',
+
+    ddbTableName: 'ng-crypto-LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
 };
+
